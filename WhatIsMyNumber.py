dfec35b897a8e4fa9b0a34b9e0b5f1f2ba0d6b22
Mamtilda <mamtathind195@gmail.com>
2023-03-09 19:24:08 +0000

    Created a guessing numbers program game

---
+++ WhatIsMyNumber.py
@@ -0,0 +1,21 @@
+import sys
+
+print("You have 5 attempts. See if you can guess it. Good luck!")
+
+for i in range (5, 0, -1):
+    user_input = int(input("Enter a number here: "))
+    if user_input == 25:
+        print("Congratulations!!! That is the correct answer.")
+        sys.exit()
+        
+    else:
+        print("Sorry, that is incorrect.")
+        
+    if i == 1:
+        print("You are all out of moves. The correct answer was 25.")
+        print("Better luck next time!")
+    
+    
+    
+   
+        
